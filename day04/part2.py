ef1602b1aed6c8ddb72837c78d411afeebdfdc8d
Oren Fromberg <orenfromberg@gmail.com>
2025-12-04 08:11:48 -0500

    day 4

---
+++ day04/part2.py
@@ -0,0 +1,61 @@
+def has_fewer_than_4_adj(grid, row, col):
+    num_adj = 0
+    if grid[row-1][col-1] == "@":
+        num_adj += 1
+    if grid[row-1][col] == "@":
+        num_adj += 1
+    if grid[row-1][col+1] == "@":
+        num_adj += 1
+    if grid[row][col+1] == "@":
+        num_adj += 1
+    if grid[row+1][col+1] == "@":
+        num_adj += 1
+    if grid[row+1][col] == "@":
+        num_adj += 1
+    if grid[row+1][col-1] == "@":
+        num_adj += 1
+    if grid[row][col-1] == "@":
+        num_adj += 1
+    return num_adj < 4
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+grid = []
+
+for line in lines:
+    line = line.strip()
+    grid.append([c for c in line])
+
+num_rows = len(grid)
+num_cols = len(grid[0])
+
+# pad grid with periods
+for row in range(num_rows):
+    grid[row].insert(0,'.')
+    grid[row].append('.')
+grid.insert(0, ['.' for _ in range(num_cols + 2) ])
+grid.append(['.' for _ in range(num_cols + 2)])
+
+total = 0
+done = False
+to_remove = set()
+
+while not done:
+    to_remove.clear()
+    for row in range(1, num_rows+1):
+        for col in range(1, num_cols+1):
+            if grid[row][col] == '@' and has_fewer_than_4_adj(grid, row, col):
+                to_remove.add((row,col))
+
+    # remove the ones to remove
+    for x,y in to_remove:
+        grid[x][y] = '.'
+
+    num_to_remove = len(to_remove)
+    if num_to_remove == 0:
+        done = True
+    else:
+        total += num_to_remove
+
+print(total)
